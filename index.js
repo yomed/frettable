2fcb1ec962e8f5c4e831b1dce1e9d36cfd3faacb
Yoni Medoff <yonimedoff@gmail.com>
2016-01-17 16:00:55 -0800

    update algo for 4 fingers

--- index.js
+++ index.js
@@ -1,41 +1,61 @@
 'use strict';
 
+function fingerSpot(fret, string) {
+    return {
+        fret: fret,
+        string: string
+    };
+};
+
+function filterAbove(note) {
+    return function (i) {
+        return i >= note;
+    }
+}
+
 function frettable(notes) {
     var fingers = [];
-    var numFingers = 3;
+    var numFingers = 4;
     var newGroup;
+    var needNewGroup;
+    var groupFret;
+    var groupString;
+    var spot;
     var diff;
-    var fingerSpot = function (string, fret) {
-        return {
-            string: string,
-            fret: fret
-        };
-    };
 
     notes.forEach(function (note, string) {
         newGroup = [];
+        spot = fingerSpot(note, string);
         if (note > 0) {
-            // try to add to current groups
-            var needNewGroup = true;
+            // try to add note to one of current finger groups
+            needNewGroup = true;
             fingers.forEach(function (fingerGroup) {
-                // if fret number matches where finger is fretted
-                if (note === fingerGroup[0].fret) {
-                    // and the diff notes are higher frets
-                    diff = notes.slice(fingerGroup[0].string + 1, string);
-                    if (diff.filter(function (i) { return i >= note;}).length === diff.length) {
-                        fingerGroup.push(fingerSpot(string, note));
+                groupFret = fingerGroup[0].fret;
+                groupString = fingerGroup[0].string;
+                // barre if fret number matches where finger is fretted on a previous note
+                // but dont barre when:
+                // * on top 5 strings and next note in sequence is a lower fret
+                // * on 5th note and third group, and last note would create fourth group
+                if (note === groupFret &&
+                    !(string < 5 && note >= notes[string + 1]) &&
+                    !(string === 4 && notes[5] !== note && fingers.length === 3)) {
+                    // check that the in between notes are on higher frets
+                    diff = notes.slice(groupString + 1, string);
+                    if (diff.filter(filterAbove(note)).length === diff.length) {
+                        // then barre this note with the rest of this finger group
+                        fingerGroup.push(spot);
                         needNewGroup = false;
                     }
                 }
             });
             // otherwise create new group
             if (needNewGroup) {
-                newGroup.push(fingerSpot(string, note));
+                newGroup.push(spot);
                 fingers.push(newGroup);
             }
         }
     });
-    return (fingers.length <= numFingers);
+    return fingers.length <= numFingers;
 }
 
 module.exports = frettable;
--- test/index.js
+++ test/index.js
@@ -11,10 +11,19 @@ describe('frettable', function () {
         expect(frettable([1, 0, 0, 0, 0, 0])).to.be.true;
         expect(frettable([1, 0, 0, 0, 0, 0])).to.be.true;
     });
-    it('should fail when there are too many unique fret positions', function () {
+    it('should fail when there are too many unique frets', function () {
         expect(frettable([1, 2, 3, 4, 5, 6])).to.be.false;
         expect(frettable([2, 2, 3, 4, 5, 6])).to.be.false;
         expect(frettable([-1, 2, 3, 4, 5, 6])).to.be.false;
         expect(frettable([0, 2, 3, 4, 5, 6])).to.be.false;
+    });
+    it('should handle weird barring', function () {
+        expect(frettable([3, 3, 4, 5, 6, 6])).to.be.true;
+        expect(frettable([3, 3, 5, 5, 5, 3])).to.be.true;
+        expect(frettable([2, 0, 0, 2, 3, 2])).to.be.true;
+    });
+    it('should fail on errant chords', function () {
+        expect(frettable([3, 3, 2, -1, 1, 3])).to.be.false; // C
+        expect(frettable([-1, 3, 2, 5, 5, 3])).to.be.false; // C
     });
 });
