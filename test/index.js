d5fe6ba4e266c4db1d5684a34d53fdbd4e435307
Yoni Medoff <yonimedoff@gmail.com>
2017-10-24 20:12:13 -0700

    try ava instead of mocha

--- test/index.js
+++ test/index.js
@@ -1,28 +1,40 @@
 'use strict';
 
-var expect = require('chai').expect;
-var frettable = require('../');
+const test = require('ava').test;
+const frettable = require('../');
 
-describe('frettable', function () {
-    it('should handle basic cases', function () {
-        expect(frettable([-1, -1, -1, -1, -1, -1])).to.equal(true);
-        expect(frettable([0, 0, 0, 0, 0, 0])).to.equal(true);
-        expect(frettable([1, 0, 0, 0, 0, 0])).to.equal(true);
-        expect(frettable([1, 0, 0, 0, 0, 0])).to.equal(true);
-    });
-    it('should fail when there are too many unique frets', function () {
-        expect(frettable([1, 2, 3, 4, 5, 6])).to.equal(false);
-        expect(frettable([2, 2, 3, 4, 5, 6])).to.equal(false);
-        expect(frettable([-1, 2, 3, 4, 5, 6])).to.equal(false);
-        expect(frettable([0, 2, 3, 4, 5, 6])).to.equal(false);
-    });
-    it('should handle weird barring', function () {
-        expect(frettable([3, 3, 4, 5, 6, 6])).to.equal(true);
-        expect(frettable([3, 3, 5, 5, 5, 3])).to.equal(true);
-        expect(frettable([2, 0, 0, 2, 3, 2])).to.equal(true);
-    });
-    it('should fail on errant chords', function () {
-        expect(frettable([3, 3, 2, -1, 1, 3])).to.equal(false); // C
-        expect(frettable([-1, 3, 2, 5, 5, 3])).to.equal(false); // C
-    });
+function verify(t, isFrettable) {
+    return chord => {
+        t.is(frettable(chord), isFrettable);
+    };
+}
+
+test('handles basic cases', t => {
+    [
+        [-1, -1, -1, -1, -1, -1],
+        [0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0]
+    ].forEach(verify(t, true));
+});
+test('handles weird barring', t => {
+    [
+        [3, 3, 4, 5, 6, 6],
+        [3, 3, 5, 5, 5, 3],
+        [2, 0, 0, 2, 3, 2]
+    ].forEach(verify(t, true));
+});
+test('fails when there are too many unique frets', t => {
+    [
+        [1, 2, 3, 4, 5, 6],
+        [2, 2, 3, 4, 5, 6],
+        [-1, 2, 3, 4, 5, 6],
+        [0, 2, 3, 4, 5, 6]
+    ].forEach(verify(t, false));
+});
+test('fails on errant chords', t => {
+    [
+        [3, 3, 2, -1, 1, 3], // C
+        [-1, 3, 2, 5, 5, 3] // C
+    ].forEach(verify(t, false));
 });
